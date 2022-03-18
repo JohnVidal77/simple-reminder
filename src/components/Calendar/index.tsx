43beebfa10b49a4770d07e779ea2d445fa9a7203
John Vidal <ba.john.vidal@gmail.com>
2022-03-18 18:49:24 -0300

    feat: listing months on calendar

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,9 +1,26 @@
 /* eslint-disable no-plusplus */
 import dayjs from 'dayjs';
+import { useState } from 'react';
 
 const WEEKDAYS_SHORT = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const MONTH_LIST = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
 export function Calendar(): JSX.Element {
+  const [selectMonth, setSelectMonth] = useState(false);
+
   const firstDayOfMonth = dayjs().startOf('month').format('ddd');
   const lastDayOfMonth = dayjs().endOf('month').format('ddd');
   const calendarDates = [];
@@ -32,24 +49,52 @@ export function Calendar(): JSX.Element {
   }
 
   return (
-    <table className="w-full">
-      <tr>
-        <th>{dayjs().format('MMMM')}</th>
-      </tr>
-      <tr className="flex w-full">
-        {WEEKDAYS_SHORT.map(day => (
-          <th className="calendar-cel" key={day}>
-            {day}
-          </th>
-        ))}
-      </tr>
-      {calendarRows.map(currentRow => (
-        <tr className="flex w-full">
-          {currentRow.map(day => (
-            <td className="calendar-cel text-center">{day}</td>
+    <div>
+      {selectMonth ? (
+        <div>
+          <ul className="flex flex-wrap">
+            {MONTH_LIST.map(month => (
+              <li className="w-1/3" key={month}>
+                <button
+                  type="button"
+                  onClick={() => {
+                    setSelectMonth(false);
+                  }}
+                >
+                  {month}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <table className="w-full">
+          <tr>
+            <th>
+              <button
+                type="button"
+                onClick={() => setSelectMonth(prev => !prev)}
+              >
+                {dayjs().format('MMMM')}
+              </button>
+            </th>
+          </tr>
+          <tr className="flex w-full">
+            {WEEKDAYS_SHORT.map(day => (
+              <th className="calendar-cel" key={day}>
+                {day}
+              </th>
+            ))}
+          </tr>
+          {calendarRows.map(currentRow => (
+            <tr className="flex w-full">
+              {currentRow.map(day => (
+                <td className="calendar-cel text-center">{day}</td>
+              ))}
+            </tr>
           ))}
-        </tr>
-      ))}
-    </table>
+        </table>
+      )}
+    </div>
   );
 }
